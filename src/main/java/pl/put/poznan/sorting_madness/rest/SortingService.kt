dd459602e4643f9599729be21b5eef6f8a6c0cf3
Klaudia <klaudia02460@gmail.com>
2022-12-18 21:42:56 +0100

    Specify parameter type of functions to Comparable and create separate methods for JsonObject

--- src/main/java/pl/put/poznan/sorting_madness/rest/SortingService.kt
+++ src/main/java/pl/put/poznan/sorting_madness/rest/SortingService.kt
@@ -1,10 +1,11 @@
 package pl.put.poznan.sorting_madness.rest
 
+import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import org.springframework.stereotype.Service
 import pl.put.poznan.sorting_madness.logic.algorithms.*
-import pl.put.poznan.sorting_madness.rest.model.*
-import java.util.*
+import pl.put.poznan.sorting_madness.rest.model.RequestModel
+import pl.put.poznan.sorting_madness.rest.model.ResponseModel
 
 @Service
 class SortingService {
@@ -14,24 +15,27 @@ class SortingService {
     ): ArrayList<ResponseModel<String>> {
         return callAlgorithms(requestModel)
     }
+
     fun sortOneDimensionalDataSetInt(
         requestModel: RequestModel<Int>
     ): ArrayList<ResponseModel<Int>> {
         return callAlgorithms(requestModel)
     }
+
     fun sortOneDimensionalDataSetFloat(
         requestModel: RequestModel<Float>
     ): ArrayList<ResponseModel<Float>> {
         return callAlgorithms(requestModel)
 
     }
+
     fun sortMultiDimensionalDataSet(
         requestModel: RequestModel<JsonObject>
-    ) : ArrayList<ResponseModel<JsonObject>> {
-        return callAlgorithms(requestModel)
+    ): ArrayList<ResponseModel<JsonObject>> {
+        return callAlgorithmsOnJsonObjects(requestModel)
     }
 
-    private fun <T> callAlgorithms(requestModel: RequestModel<T>): ArrayList<ResponseModel<T>> {
+    private fun <T : Comparable<T>> callAlgorithms(requestModel: RequestModel<T>): ArrayList<ResponseModel<T>> {
         return if (requestModel.algorithm == null) {
             runAllAlgorithms(
                 numOfIterations = requestModel.iterationNumber,
@@ -50,7 +54,26 @@ class SortingService {
         }
     }
 
-    private fun <T> runAlgorithm(
+    private fun callAlgorithmsOnJsonObjects(requestModel: RequestModel<JsonObject>): ArrayList<ResponseModel<JsonObject>> {
+        return if (requestModel.algorithm == null) {
+            runAllAlgorithmsOnJsonObjects(
+                numOfIterations = requestModel.iterationNumber,
+                property = requestModel.property,
+                data = requestModel.data,
+                order = requestModel.sortingOrder
+            )
+        } else {
+            runAlgorithmOnJsonObject(
+                numOfIterations = requestModel.iterationNumber,
+                property = requestModel.property,
+                data = requestModel.data,
+                algorithm = requestModel.algorithm,
+                order = requestModel.sortingOrder
+            )
+        }
+    }
+
+    private fun <T : Comparable<T>> runAlgorithm(
         algorithm: Algorithm,
         numOfIterations: Int?,
         order: SortingOrder?,
@@ -68,18 +91,20 @@ class SortingService {
 
         val sortedArray = ArrayList<T>()
         sortedDataResponse.sortedData.forEach { sortedArray.add(it) }
-        arrayList.add(ResponseModel(
-            sortedData = sortedArray,
-            property = property,
-            sortingOrder = order ?: SortingOrder.ASCENDING,
-            iterationNumber = numOfIterations ?: 0,
-            algorithm = algorithm,
-            time = sortedDataResponse.time.toString() + "nanoseconds"
-         ))
+        arrayList.add(
+            ResponseModel(
+                sortedData = sortedArray,
+                property = property,
+                sortingOrder = order ?: SortingOrder.ASCENDING,
+                iterationNumber = numOfIterations ?: 0,
+                algorithm = algorithm,
+                time = sortedDataResponse.time.toString() + "nanoseconds"
+            )
+        )
         return arrayList
     }
 
-    private fun <T> runAllAlgorithms(
+    private fun <T : Comparable<T>> runAllAlgorithms(
         numOfIterations: Int?,
         order: SortingOrder?,
         property: String?,
@@ -100,8 +125,105 @@ class SortingService {
         return arrayList
     }
 
+    private fun runAllAlgorithmsOnJsonObjects(
+        numOfIterations: Int?,
+        order: SortingOrder?,
+        property: String?,
+        data: ArrayList<JsonObject>
+    ): ArrayList<ResponseModel<JsonObject>> {
+        val arrayList = ArrayList<ResponseModel<JsonObject>>()
+        for (algo in Algorithm.values()) {
+            arrayList.add(
+                runAlgorithmOnJsonObject(
+                    algorithm = algo,
+                    numOfIterations = numOfIterations,
+                    order = order,
+                    property = property,
+                    data = data
+                )[0]
+            )
+        }
+        return arrayList
+    }
+
+
+    private fun runAlgorithmOnJsonObject(
+        algorithm: Algorithm,
+        numOfIterations: Int?,
+        order: SortingOrder?,
+        property: String?,
+        data: ArrayList<JsonObject>
+    ): ArrayList<ResponseModel<JsonObject>> {
+        val sortable: Sortable = resolveAlgorithm(algorithm)
+        val arrayList = ArrayList<ResponseModel<JsonObject>>()
+        var listOfStrings = emptyList<String>()
+        var listOfNumbers = emptyList<Int>()
+        var time: Long? = 0
+        if (data.isNotEmpty()) {
+            val element = data[0].get(property)
+            when (getJsonElementType(element)) {
+                ValueType.NUMBER -> {
+                    listOfNumbers = data.map { it.asInt }
+                }
+                ValueType.STRING -> {
+                    listOfStrings = data.map { it.asString }
+                }
+                ValueType.BOOLEAN -> {}
+                ValueType.NULL -> {}
+            }
+
+        }
+        val sortedArray = ArrayList<JsonObject>()
+        if (numOfIterations != null && numOfIterations > 0) {
+            if (listOfStrings.isNotEmpty()) sortable.run(listOfStrings, numOfIterations, order).run {
+                this.sortedData.forEach { value ->
+                    data.find { it.get(property).asString == value }?.let {
+                        sortedArray.add(it)
+                    }
+                }
+                time = this.time
+            }
+            else if (listOfNumbers.isNotEmpty()) sortable.run(listOfNumbers, numOfIterations, order).run {
+                this.sortedData.forEach { value ->
+                    data.find { it.get(property).asInt == value }?.let {
+                        sortedArray.add(it)
+                    }
+                }
+                time = this.time
+            }
+        } else {
+            if (listOfStrings.isNotEmpty()) sortable.run(listOfStrings, order).run {
+                this.sortedData.forEach { value ->
+                    data.find { it.get(property).asString == value }?.let {
+                        sortedArray.add(it)
+                    }
+                }
+                time = this.time
+            }
+            else if (listOfNumbers.isNotEmpty()) sortable.run(listOfNumbers, order).run {
+                this.sortedData.forEach { value ->
+                    data.find { it.get(property).asInt == value }?.let {
+                        sortedArray.add(it)
+                    }
+                }
+                time = this.time
+            }
+        }
+        arrayList.add(
+            ResponseModel(
+                sortedData = sortedArray,
+                property = property,
+                sortingOrder = order ?: SortingOrder.ASCENDING,
+                iterationNumber = numOfIterations ?: 0,
+                algorithm = algorithm,
+                time = time.toString() + "nanoseconds"
+            )
+        )
+        return arrayList
+    }
+
     private fun resolveAlgorithm(algorithm: Algorithm): Sortable {
-        return when(algorithm) {
+        return when (algorithm) {
             Algorithm.BUBBLE_SORT -> BubbleSort()
             Algorithm.HEAP_SORT -> HeapSort()
             Algorithm.INSERTION_SORT -> InsertionSort()
@@ -111,4 +233,24 @@ class SortingService {
         }
     }
 
+    private fun getJsonElementType(element: JsonElement): ValueType {
+        if (element.isJsonNull)
+            return ValueType.NULL;
+
+        if (element.isJsonPrimitive) {
+            if (element.asJsonPrimitive.isString)
+                return ValueType.STRING;
+            if (element.asJsonPrimitive.isNumber) {
+                return ValueType.NUMBER;
+            }
+            if (element.asJsonPrimitive.isBoolean)
+                return ValueType.BOOLEAN;
+        }
+        return ValueType.NULL
+    }
+
+}
+
+enum class ValueType {
+    NULL, NUMBER, STRING, BOOLEAN
 }
